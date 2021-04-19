438f0cd24a4172e020466c8d095e8692f1d3942e
yw3563 <71254905+yw3563@users.noreply.github.com>
2021-04-19 15:53:34 +0800

    final

---
+++ project04/pictures.js
@@ -0,0 +1,645 @@
+const picArr =
+[{
+  name:"Hannya tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Hannya",
+  image:"assets/001.png"
+},
+{
+  name:"Tiger tattoo",
+  catagory:"Animals",
+  keyword:"Tiger",
+  image:"assets/002.png"
+},
+{
+  name:"Carp tattoo",
+  catagory:"Animals",
+  keyword:"Carp",
+  image:"assets/003.png"
+},
+{
+  name:"Snake tattoo",
+  catagory:"Animals",
+  keyword:"Snake",
+  image:"assets/004.png"
+},
+{
+  name:"Dragon tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Dragon",
+  image:"assets/005.png"
+},
+{
+  name:"Dragon tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Dragon",
+  image:"assets/006.jpg"
+},
+{
+  name:"Phoenix tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Phoenix",
+  image:"assets/007.jpg"
+},
+{
+  name:"Samurai tattoo",
+  catagory:"Characters",
+  keyword:"Samurai",
+  image:"assets/008.jpg"
+},
+{
+  name:"Geisha tattoo",
+  catagory:"Characters",
+  keyword:"Geisha",
+  image:"assets/009.jpg"
+},
+{
+  name:"Carp tattoo",
+  catagory:"Animals",
+  keyword:"Carp",
+  image:"assets/010.jpg"
+},
+{
+  name:"Flower tattoo",
+  catagory:"Others",
+  keyword:"Flower",
+  image:"assets/011.jpg"
+},
+{
+  name:"Geisha tattoo",
+  catagory:"Characters",
+  keyword:"Geisha",
+  image:"assets/012.jpg"
+},
+{
+  name:"Tower tattoo",
+  catagory:"Others",
+  keyword:"Tower",
+  image:"assets/013.jpg"
+},
+{
+  name:"Hannya tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Hannya",
+  image:"assets/014.jpg"
+},
+{
+  name:"Samurai tattoo",
+  catagory:"Characters",
+  keyword:"Samurai",
+  image:"assets/015.jpg"
+},
+{
+  name:"Dragon tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Dragon",
+  image:"assets/016.jpg"
+},
+{
+  name:"Carp tattoo",
+  catagory:"Animals",
+  keyword:"Carp",
+  image:"assets/017.jpg"
+},
+{
+  name:"Phoenix tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Phoenix",
+  image:"assets/018.jpg"
+},
+{
+  name:"Phoenix tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Phoenix",
+  image:"assets/019.jpg"
+},
+{
+  name:"Momo-taro tattoo",
+  catagory:"Characters",
+  keyword:"Momo-taro",
+  image:"assets/020.jpg"
+},
+{
+  name:"Fudo Myoo tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Fudo Myoo",
+  image:"assets/021.jpg"
+},
+{
+  name:"Toad tattoo",
+  catagory:"Animals",
+  keyword:"Toad",
+  image:"assets/022.jpg"
+},
+{
+  name:"Toad tattoo",
+  catagory:"Animals",
+  keyword:"Toad",
+  image:"assets/023.jpg"
+},
+{
+  name:"Samurai tattoo",
+  catagory:"Characters",
+  keyword:"Samurai",
+  image:"assets/024.jpg"
+},
+{
+  name:"Snake tattoo",
+  catagory:"Animals",
+  keyword:"Snake",
+  image:"assets/025.jpg"
+},
+{
+  name:"Crane tattoo",
+  catagory:"Animals",
+  keyword:"Crane",
+  image:"assets/026.jpg"
+},
+{
+  name:"Fudo Myoo tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Fudo Myoo",
+  image:"assets/027.jpg"
+},
+{
+  name:"Kirin tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Kirin",
+  image:"assets/028.jpg"
+},
+{
+  name:"Kirin tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Kirin",
+  image:"assets/029.jpg"
+},
+{
+  name:"Kirin tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Kirin",
+  image:"assets/030.jpg"
+},
+{
+  name:"Tiger tattoo",
+  catagory:"Animals",
+  keyword:"Tiger",
+  image:"assets/031.jpg"
+},
+{
+  name:"Guardian Lions tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Guardian Lions",
+  image:"assets/032.jpg"
+},
+{
+  name:"Kin-taro tattoo",
+  catagory:"Characters",
+  keyword:"Kin-taro",
+  image:"assets/033.jpg"
+},
+{
+  name:"Daruma tattoo",
+  catagory:"Others",
+  keyword:"Daruma",
+  image:"assets/034.jpg"
+},
+{
+  name:"Tengu tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Tengu",
+  image:"assets/035.jpg"
+},
+{
+  name:"Fujin/Japanese God of the Wind tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Fujin",
+  image:"assets/036.jpg"
+},
+{
+  name:"Fujin and Raijin tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Fujin and Raijin",
+  image:"assets/037.jpg"
+},
+{
+  name:"Fujin and Raijin tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Fujin and Raijin",
+  image:"assets/038.jpg"
+},
+{
+  name:"Guardian Lions tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Guardian Lions",
+  image:"assets/039.jpg"
+},
+{
+  name:"Fox tattoo",
+  catagory:"Animals",
+  keyword:"Fox",
+  image:"assets/040.jpg"
+},
+{
+  name:"Nine-tail Fox tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Nine-tail Fox",
+  image:"assets/041.jpg"
+},
+{
+  name:"Snake tattoo",
+  catagory:"Animals",
+  keyword:"Snake",
+  image:"assets/042.jpg"
+},
+{
+  name:"Nue tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Nue",
+  image:"assets/043.jpg"
+},
+{
+  name:"Nue tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Nue",
+  image:"assets/044.jpg"
+},
+{
+  name:"Hannya tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Hannya",
+  image:"assets/045.jpg"
+},
+{
+  name:"Hannya tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Hannya",
+  image:"assets/046.jpg"
+},
+{
+  name:"Tofu-kozo tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Tofu-kozo",
+  image:"assets/047.jpg"
+},
+{
+  name:"Tofu-kozo tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Tofu-kozo",
+  image:"assets/048.jpg"
+},
+{
+  name:"The Umbrella Demon tattoo",
+  catagory:"Gods and Demons",
+  keyword:"The Umbrella Demon",
+  image:"assets/049.jpg"
+},
+{
+  name:"The Spider Woman tattoo",
+  catagory:"Gods and Demons",
+  keyword:"The Spider Woman",
+  image:"assets/050.jpg"
+},
+{
+  name:"Crane tattoo",
+  catagory:"Animals",
+  keyword:"Crane",
+  image:"assets/051.jpg"
+},
+{
+  name:"Crane tattoo",
+  catagory:"Animals",
+  keyword:"Crane",
+  image:"assets/052.jpg"
+},
+{
+  name:"Tengu tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Tengu",
+  image:"assets/053.jpg"
+},
+{
+  name:"Kappa tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Kappa",
+  image:"assets/054.jpg"
+},
+{
+  name:"Raijin/Japanese God of the Thunder tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Raijin",
+  image:"assets/055.jpg"
+},
+{
+  name:"Guardian Lions tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Guardian Lions",
+  image:"assets/056.jpg"
+},
+{
+  name:"Carp tattoo",
+  catagory:"Animals",
+  keyword:"Carp",
+  image:"assets/057.jpg"
+},
+{
+  name:"Phoenix tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Phoenix",
+  image:"assets/058.jpg"
+},
+{
+  name:"Snake tattoo",
+  catagory:"Animals",
+  keyword:"Snake",
+  image:"assets/059.jpg"
+},
+{
+  name:"Fudo Myoo tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Fudo Myoo",
+  image:"assets/060.jpg"
+},
+{
+  name:"Japanese Crab tattoo",
+  catagory:"Animals",
+  keyword:"Japanese Crab",
+  image:"assets/061.jpg"
+},
+{
+  name:"Nue tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Nue",
+  image:"assets/062.jpg"
+},
+{
+  name:"Flower tattoo",
+  catagory:"Others",
+  keyword:"Flower",
+  image:"assets/063.jpg"
+},
+{
+  name:"Flower tattoo",
+  catagory:"Others",
+  keyword:"Flower",
+  image:"assets/064.jpg"
+},
+{
+  name:"Geisha tattoo",
+  catagory:"Characters",
+  keyword:"Geisha",
+  image:"assets/065.jpg"
+},
+{
+  name:"Toad tattoo",
+  catagory:"Animals",
+  keyword:"Toad",
+  image:"assets/066.jpg"
+},
+{
+  name:"Monkey tattoo",
+  catagory:"Animals",
+  keyword:"Monkey",
+  image:"assets/067.jpg"
+},
+{
+  name:"Bakeneko tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Bakeneko",
+  image:"assets/068.jpg"
+},
+{
+  name:"Daruma tattoo",
+  catagory:"Others",
+  keyword:"Daruma",
+  image:"assets/069.jpg"
+},
+{
+  name:"Crane tattoo",
+  catagory:"Animals",
+  keyword:"Crane",
+  image:"assets/070.jpg"
+},
+{
+  name:"Hannya tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Hannya",
+  image:"assets/071.jpg"
+},
+{
+  name:"Dragon tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Dragon",
+  image:"assets/072.jpg"
+},
+{
+  name:"Carp tattoo",
+  catagory:"Animals",
+  keyword:"Carp",
+  image:"assets/074.jpg"
+},
+{
+  name:"Tiger tattoo",
+  catagory:"Animals",
+  keyword:"Tiger",
+  image:"assets/075.jpg"
+},
+{
+  name:"Guardian Lions tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Guardian Lions",
+  image:"assets/076.jpg"
+},
+{
+  name:"Tower tattoo",
+  catagory:"Others",
+  keyword:"Tower",
+  image:"assets/077.jpg"
+},
+{
+  name:"Samurai tattoo",
+  catagory:"Characters",
+  keyword:"Samurai",
+  image:"assets/078.jpg"
+},
+{
+  name:"Fudo Myoo tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Fudo Myoo",
+  image:"assets/079.jpg"
+},
+{
+  name:"Dragon tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Dragon",
+  image:"assets/080.jpg"
+},
+{
+  name:"Hannya tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Hannya",
+  image:"assets/081.jpg"
+},
+{
+  name:"Geisha tattoo",
+  catagory:"Characters",
+  keyword:"Geisha",
+  image:"assets/082.jpg"
+},
+{
+  name:"Flower tattoo",
+  catagory:"Others",
+  keyword:"Flower",
+  image:"assets/083.jpg"
+},
+{
+  name:"Geisha tattoo",
+  catagory:"Characters",
+  keyword:"Geisha",
+  image:"assets/084.jpg"
+},
+{
+  name:"Geisha tattoo",
+  catagory:"Characters",
+  keyword:"Geisha",
+  image:"assets/085.jpg"
+},
+{
+  name:"Geisha tattoo",
+  catagory:"Characters",
+  keyword:"Geisha",
+  image:"assets/086.jpg"
+},
+{
+  name:"Phoenix tattoo",
+  catagory:"Mythological Creatures",
+  keyword:"Phoenix",
+  image:"assets/087.jpg"
+},
+{
+  name:"Carp tattoo",
+  catagory:"Animals",
+  keyword:"Carp",
+  image:"assets/088.jpg"
+},
+{
+  name:"Toad tattoo",
+  catagory:"Animals",
+  keyword:"Toad",
+  image:"assets/089.jpg"
+},
+{
+  name:"Toad tattoo",
+  catagory:"Animals",
+  keyword:"Toad",
+  image:"assets/090.jpg"
+},
+{
+  name:"Tiger tattoo",
+  catagory:"Animals",
+  keyword:"Tiger",
+  image:"assets/091.jpg"
+},
+{
+  name:"Tiger tattoo",
+  catagory:"Animals",
+  keyword:"Tiger",
+  image:"assets/092.jpg"
+},
+{
+  name:"Japanese Raccoon Dog tattoo",
+  catagory:"Animals",
+  keyword:"Japanese Raccoon Dog",
+  image:"assets/093.jpg"
+},
+{
+  name:"Japanese Raccoon Dog tattoo",
+  catagory:"Animals",
+  keyword:"Japanese Raccoon Dog",
+  image:"assets/094.jpg"
+},
+{
+  name:"Japanese Raccoon Dog tattoo",
+  catagory:"Animals",
+  keyword:"Japanese Raccoon Dog",
+  image:"assets/095.jpg"
+},
+{
+  name:"Raijin/Japanese God of the Thunder tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Raijin",
+  image:"assets/096.jpg"
+},
+{
+  name:"Tengu tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Tengu",
+  image:"assets/097.jpg"
+},
+{
+  name:"Tengu tattoo",
+  catagory:"Gods and Demons",
+  keyword:"Tengu",
+  image:"assets/098.jpg"
+},
+{
+  name:"Momo-taro tattoo",
+  catagory:"Characters",
+  keyword:"Momo-taro",
+  image:"assets/099.jpg"
+},
+{
+  name:"Momo-taro tattoo",
+  catagory:"Characters",
+  keyword:"Momo-taro",
+  image:"assets/100.jpg"
+},
+];
+
+function findPicByTag(tag){
+
+  document.getElementById('picArea').innerHTML = "";
+
+  let picArea=document.getElementById("picArea");
+  for(i = 0; i < picArr.length; i++){
+
+    if(picArr[i].catagory == tag)
+      {
+        let newPic=document.createElement("img");
+        newPic.src = picArr[i].image;
+
+        newPic.width="180";
+        newPic.height="180";
+        picArea.appendChild(newPic);
+        newPic.onclick=function()
+        {
+           window.open(this.src);
+        }
+          }
+        }
+}
+
+function findPic(){
+
+  var tagInput=document.getElementById('picTagText').value;
+
+	   document.getElementById('picArea').innerHTML = "";
+
+	    let picArea=document.getElementById("picArea");
+	     for(var i=0;i<picArr.length;i++)
+          {
+
+            if(picArr[i].keyword.toLowerCase() == tagInput.toLowerCase()){
+    	         let newPic=document.createElement("img");
+	              newPic.src = picArr[i].image;
+
+ 	  	           newPic.width="180";
+  	  	           newPic.height="180";
+  	  	             picArea.appendChild(newPic);
+                     newPic.onclick=function()
+                     {
+                        window.open(this.src);
+                     }
+                   }
+                 }
+
+}
